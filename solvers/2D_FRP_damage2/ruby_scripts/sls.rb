f7ef3c04abf15e7387d56abf35ad53071eeac5de
Nobuhito Ibaraki <niba1122@gmail.com>
2015-11-06 18:15:19 +0900

    SLSの収束判定プログラムの改良、修正

--- solvers/2D_FRP_damage2/ruby_scripts/sls.rb
+++ solvers/2D_FRP_damage2/ruby_scripts/sls.rb
@@ -3,7 +3,7 @@ require './classes.rb'
 
 class SLS
   include Math
-  attr_accessor :convergence_step
+  attr_accessor :convergence_step,:dexp,:threshold
   
   N_TIMES_CONVERGENCE = 3
 
@@ -12,26 +12,27 @@ class SLS
     @data = data
     @max_n = max_n
     @dexp = {}
-    @exp = [0.0]
+    @exp = {}
     @convergence_step = nil
 
-    threshold = @data[0...@n_models_1step].sd/(@data[0...@n_models_1step].exp*sqrt(@max_n))
+    @exp[1] = @data[0...@n_models_1step].exp
+    @threshold = @data[0...@n_models_1step].sd/(@data[0...@n_models_1step].exp*sqrt(@max_n))
     p @n_models_1step
     p @max_n
-    p threshold
+    p @threshold
     p @data.length
 
     n_step = @data.length/@n_models_1step
 
     count = 0
-    [*1..n_step].each do |i|
-      @exp << @data[0...i*@n_models_1step].exp
-      @dexp[i] = (@exp[i] - @exp[i-1]).abs
+    [*2..n_step].each do |i|
+      @exp[i] = @data[0...i*@n_models_1step].exp
+      @dexp[i] = ((@exp[i] - @exp[i-1])/@exp[i-1]).abs
 
-      if @dexp[i] < threshold
+      if @dexp[i] < @threshold
         count += 1
       end
-      puts "#{@dexp[i]}, #{threshold}, #{count}"
+      puts "#{@dexp[i]}, #{@threshold}, #{count}"
       if count == N_TIMES_CONVERGENCE
         @convergence_step = i
         break
@@ -55,11 +56,26 @@ end
 #main
 DATA_PATH = '../../../models/gfrp_damage/damage_ratios.csv'
 
-# main
 io = File.open(DATA_PATH)
 
 data = CSV.new(io,headers: true).read
 
-sls = SLS.new(data['damage_ratioT'].to_a,10)
-p sls.convergence_step
+# damage_ratioT
+puts "----- damage_ratioT -----"
+sls_damage_ratioT = SLS.new(data['damage_ratioT'].to_a,100)
+p sls_damage_ratioT.convergence_step*100
+p sls_damage_ratioT.dexp
 
+sls_damage_ratioT.dexp.each do |step,dexp|
+  puts "#{step},#{dexp}"
+end
+
+# damage_ratioTZ
+puts "----- damage_ratioTZ -----"
+sls_damage_ratioTZ = SLS.new(data['damage_rationTZ'].to_a,100)
+p sls_damage_ratioTZ.convergence_step*100
+p sls_damage_ratioTZ.dexp
+
+sls_damage_ratioTZ.dexp.each do |step,dexp|
+  puts "#{step},#{dexp}"
+end
