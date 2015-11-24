f427296e25da4aab7a34f4696c3b52c35d5b73b1
Nobuhito Ibaraki <niba1122@gmail.com>
2015-11-24 17:37:08 +0900

    荷重と変異の関係を出力するスクリプト追加

---
+++ solvers/2D_FRP_damage2/ruby_scripts/p_d_damaged.rb
@@ -0,0 +1,33 @@
+#
+#   p_d_damaged.rb
+#   
+#   (c) 2015 Nobuhito Ibaraki
+#
+#
+#   各モデルの損傷時の荷重、変位を出力するプログラム
+#
+
+require './classes.rb'
+
+# config
+
+DU = 8.89e-6 # 変位増分
+N_MODELS = 2000 # モデル数
+MODEL_PATH = '../../../models/gfrp_damage'
+
+# main
+damage_ratios = CSV.table("#{MODEL_PATH}/damage_ratios.csv",headers: true)
+
+io = File.open("#{MODEL_PATH}/p_d_damaged.csv","w")
+io.puts "u_damaged,f_damaged"
+
+N_MODELS.times do |i|
+  reaction_forces = CSV.table("#{MODEL_PATH}/model#{i+1}/model#{i+1}.csv",headers: true)
+
+  reaction_force_left = reaction_forces[0][0].gsub('D','e').to_f # 1ステップ目の左端荷重
+
+  io.puts "#{damage_ratios[i][:damaged_u]}, #{reaction_force_left * damage_ratios[i][:damaged_u] / DU }"
+  
+end
+
+io.close
