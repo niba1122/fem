ce1999ce77445e53263b9a70fc754d66c9fbc8ac
Nobuhito Ibaraki <niba1122@gmail.com>
2015-12-07 14:41:53 +0900

    積層なしの場合の設定をコメントアウト

--- solvers/2D_FRP_damage2/ruby_scripts/config.rb
+++ solvers/2D_FRP_damage2/ruby_scripts/config.rb
@@ -1,3 +1,3 @@
 MODEL_PATH = '../../../models/gfrp_damage/wvf_wl' # 最後スラッシュなし
-MODEL_PATH = '../../../models/gfrp_damage/wvf_wol' # 最後スラッシュなし
+#MODEL_PATH = '../../../models/gfrp_damage/wvf_wol' # 最後スラッシュなし
 N_MODELS = 2000
